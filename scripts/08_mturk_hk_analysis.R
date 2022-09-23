918adaf8f31b22dbc8dff36f04738e3d214f3837
danweitzel <weitzel.daniel@gmail.com>
2022-09-23 15:08:52 -0600

    mturk_hk ready for Stata

--- scripts/08_mturk_hk_analysis.R
+++ scripts/08_mturk_hk_analysis.R
@@ -12,7 +12,7 @@ setwd("partisan-gaps")  # folder
 # Load libraries
  
 # Read in recoded data 
-source("scripts/07_mturk_hk_recode.R")
+source("scripts/07_mturk_hk_hk_recode.R")
 
 # 4. Blind Guessing, Cheating, Inference, Expressive Responding
 # Exploiting the 'inference battery'
@@ -28,31 +28,31 @@ source("scripts/07_mturk_hk_recode.R")
 stems_inf       <- c("oc", "bc", "aca2c", "dtc")
 
 # Proportion Correct 
-inf_pc 			<- sapply(mturk[mturk$probe=='closed',  paste0(stems_inf, "_pc")], function(x) mean(x))
-inf_pc_se   <- sapply(inf_pc, function(x) se_prop(x, nrow(mturk[mturk$probe=='closed',])))
+inf_pc 			<- sapply(mturk_hk[mturk_hk$probe=='closed',  paste0(stems_inf, "_pc")], function(x) mean(x))
+inf_pc_se   <- sapply(inf_pc, function(x) se_prop(x, nrow(mturk_hk[mturk_hk$probe=='closed',])))
 
 # Actual Knowledge
-inf_ak 			<- sapply(mturk[mturk$probe=='closed', paste0(stems_inf, "_ak")], function(x) mean(x))
-inf_ak_se   <- sapply(inf_ak, function(x) se_prop(x, nrow(mturk[mturk$probe=='closed',])))
+inf_ak 			<- sapply(mturk_hk[mturk_hk$probe=='closed', paste0(stems_inf, "_ak")], function(x) mean(x))
+inf_ak_se   <- sapply(inf_ak, function(x) se_prop(x, nrow(mturk_hk[mturk_hk$probe=='closed',])))
 
 # Cheating
-inf_ch      <- sapply(mturk[mturk$probe=='closed', paste0(stems_inf, "_ch")], function(x) mean(x))
-inf_ch_se		<- sapply(inf_ch, function(x) se_prop(x, nrow(mturk[mturk$probe=='closed',])))
+inf_ch      <- sapply(mturk_hk[mturk_hk$probe=='closed', paste0(stems_inf, "_ch")], function(x) mean(x))
+inf_ch_se		<- sapply(inf_ch, function(x) se_prop(x, nrow(mturk_hk[mturk_hk$probe=='closed',])))
 
 # Guessing
-inf_gu       	<- sapply(mturk[mturk$probe=='closed', paste0(stems_inf, "_gu")], function(x) mean(x))
-inf_gu_se    	<- sapply(inf_gu, function(x) se_prop(x, nrow(mturk[mturk$probe=='closed',])))
+inf_gu       	<- sapply(mturk_hk[mturk_hk$probe=='closed', paste0(stems_inf, "_gu")], function(x) mean(x))
+inf_gu_se    	<- sapply(inf_gu, function(x) se_prop(x, nrow(mturk_hk[mturk_hk$probe=='closed',])))
 
 # Inference
-inf_in      	<- sapply(mturk[mturk$probe=='closed', paste0(stems_inf, "_in")], function(x) mean(x))
-inf_in_se    	<- sapply(inf_in, function(x) se_prop(x, nrow(mturk[mturk$probe=='closed',])))
+inf_in      	<- sapply(mturk_hk[mturk_hk$probe=='closed', paste0(stems_inf, "_in")], function(x) mean(x))
+inf_in_se    	<- sapply(inf_in, function(x) se_prop(x, nrow(mturk_hk[mturk_hk$probe=='closed',])))
 
 # Expressive			 
-inf_ex     		<- sapply(mturk[mturk$probe=='closed', paste0(stems_inf, "_ex")], function(x) mean(x))
-inf_ex_se    	<- sapply(inf_ex, function(x) se_prop(x, nrow(mturk[mturk$probe=='closed',])))	
+inf_ex     		<- sapply(mturk_hk[mturk_hk$probe=='closed', paste0(stems_inf, "_ex")], function(x) mean(x))
+inf_ex_se    	<- sapply(inf_ex, function(x) se_prop(x, nrow(mturk_hk[mturk_hk$probe=='closed',])))	
 
 inference 		<- data.frame(inf_pc, inf_pc_se, inf_ak, inf_ak_se, inf_ch, inf_ch_se, 
                             inf_gu, inf_gu_se, inf_in, inf_in_se, inf_ex, inf_ex_se)		 
 
 # Generate csv
-write.csv(inference, file = "res/tab8_mturk_inference.csv", row.names = F)
+write.csv(inference, file = "res/tab8_mturk_hk_inference.csv", row.names = F)
