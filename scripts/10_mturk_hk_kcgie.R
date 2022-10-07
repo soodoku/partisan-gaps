939e389601c2660ef5d770766b065b9edc949af9
Daniel Weitzel <weitzel.daniel@gmail.com>
2022-10-07 13:01:06 -0600

    adding csv files

--- scripts/10_mturk_hk_kcgie.R
+++ scripts/10_mturk_hk_kcgie.R
@@ -160,3 +160,8 @@ mturk_hk_scale <-
 
 ## Remove original file which is not required anymore
 rm(mturk_hk)
+
+## Export files for Stata analysis
+write_csv(mturk_hk_closed_correct, "data/mturk_hk/mturk_hk_MC.csv")
+write_csv(mturk_hk_probes, "data/mturk_hk/mturk_hk_PROBES.csv")
+write_csv(mturk_hk_scale, "data/mturk_hk/mturk_hk_LIKERT.csv")
