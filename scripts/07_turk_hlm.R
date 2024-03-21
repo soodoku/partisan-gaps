548ca1b37c6ea468e274a2a92f5b59c386ca1b14
***** <gsood07@gmail.com>
2024-03-20 17:14:20 -0700

    hlm

--- scripts/07_turk_hlm.R
+++ scripts/07_turk_hlm.R
@@ -23,10 +23,14 @@ mt$item1 <- ifelse(mt$item == "Correct", 1, 0)
 
 # Model
 m1 <- lmer(item1 ~ rep*survey1 + item_type + (rep*survey1| id), data = mt)
-m2 <- lmer(item1 ~ rep*survey1 + (rep*survey1 | id) + (rep*survey1| item_type), data = mt)
-m3 <- lmer(item1 ~ rep*survey1 + (survey1| rep) + (1| survey1), data = mt)
+#m2 <- lmer(item1 ~ rep*survey1 + (rep*survey1 | id) + (rep*survey1| item_type), data = mt)
+# varying intercepts over conditions and varying slopes over your congenial variable (R3)
+#m3 <- lmer(item1 ~ rep*survey1 + (1 + rep*survey1| rep) + (1| survey1) + (rep*survey1 | id), data = mt)
+# if you follow gelman: http://www.stat.columbia.edu/~gelman/research/unpublished/multiple2.pdf
+# same as m1
+m4 <- lmer(item1 ~ rep*survey1 + (1 + survey1| item_type) + (rep*survey1 | id), data = mt)
 
-stargazer(m1, m2, m3, 
+stargazer(m1, m4, 
           type = "latex", 
           title = "Comparison of Linear Mixed-Effects Models", 
           align = TRUE,
@@ -36,7 +40,7 @@ stargazer(m1, m2, m3,
           covariate.labels = c("Congenial", "CUD", "FSR", "IMC",
                                "Congenial × CUD", "Congenial × FSR", "Congenial × IMC", 
                                "Constant"),
-          column.labels = c("Model 1", "Model 2", "Model 3"),
+          column.labels = c("Model 1", "Model 2"),
           omit.stat = c("LL", "ser", "f", "AIC", "BIC"),
           omit.table.layout = "n",
           header = FALSE,
